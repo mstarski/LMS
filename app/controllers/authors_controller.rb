2d9256a1a58b3081f2be35c711c12eaf17eab721
Michał Starski <michalstarski99@gmail.com>
2019-05-11 14:52:54 +0200

    Updated rails version, added update and destroy methods to the authors controller

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -6,4 +6,31 @@ class AuthorsController < ApplicationController
     def show
         @author = Author.find(params[:id])
     end
+
+    def edit
+        @author = Author.find(params[:id])
+    end
+
+    def update
+        @author = Author.find(params[:id])
+        if @author.update_attributes(author_params)
+            render file: '/app/views/authors/edit_success.html.erb'
+        else
+            render file: '/app/views/authors/edit_success.html.erb'
+        end
+    end
+
+    def destroy
+        @author = Author.find(params[:id])
+        if @author.destroy
+            render file: '/app/views/authors/delete_success'
+        else
+            render file: '/app/views/authors/delete_failed'
+        end
+    end
+
+    private
+        def author_params
+            params.require(:author).permit(:name, :surname)
+        end
 end
